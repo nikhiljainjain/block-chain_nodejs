0b090687c5c791fbb8e70ea730e275032d0537ee
NIKHIL JAIN <nikhiljain.ashajain@gmail.com>
2019-09-07 12:00:24 +0530

    app -> routes

--- app/app.js
+++ app/app.js
@@ -1,26 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let pageRouter = require('./routes/page');
 
-const P2pServer = require('./p2p-server');
-const Blockchain = require('../blockchain');
-const Wallet = require('../wallet');
-const TransactionPool = require('../wallet/transaction-pool');
-const Miner = require('./miner');
-
-const bc = new Blockchain();
-const wallet = new Wallet();
-const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc, tp);
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
-const miner = new Miner(bc, tp, wallet, p2pServer);
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,43 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/block', (req, res)=>{
-	//console.log(bc.chain);
-	res.json(bc.chain);
-});
-
-app.post('/mine', (req, res)=>{
-	//console.log(data);
-	const nwBlock = bc.addBlock(req.body.data);
-	console.log(`New block added  ${nwBlock.toString()}`);
-	p2pServer.syncChain();
-	res.status(302).redirect('/block');
-});
-
-app.get('/transaction', (req, res)=>{
-	res.json(tp.transactions);
-});
-
-app.post('/transact', (req, res)=>{
-	const { recipient, amount } = req.body;
-	const transaction = wallet.createTransaction(recipient, amount, tp);
-	//console.log(tp, transaction);
-	p2pServer.broadcastTransaction(transaction);
-	res.status(302).redirect('/transaction');
-});
-
-app.get('/mine-transactions', (req, res)=>{
-	const block = miner.mine();
-	console.log(`New block added: ${block}`);
-	res.status(302).redirect('/block');
-});
-
-app.get('/public-key', (req, res)=>{
-	res.json({publicKey: wallet.publicKey});
-});
-
-/*app.use('/', indexRouter);
-app.use('/users', usersRouter);*/
+app.use('/', indexRouter);
+app.use('/pages', pageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,20 +3,61 @@ const router = express.Router();
 const Blockchain = require('../../blockchain');
 const bc = new Blockchain();
 
-/* GET home page. */
+const P2pServer = require('./p2p-server');
+const Blockchain = require('../blockchain');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
+
+const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/block', (req, res, next)=>{
-  res.json(bc.chain);
+router.get('/block', (req, res)=>{
+	//console.log(bc.chain);
+	res.json(bc.chain);
 });
 
-router.post('/mine', (req, res, next)=>{
-  const nwBlock = bc.addBlock(req.body.data);
-  console.log(`New block added  ${nwBlock.toString()}`);
-
-  res.redirect('/block');
+router.get('/transaction', (req, res)=>{
+	res.json(tp.transactions);
 });
 
+router.get('/mine-transactions', (req, res)=>{
+	const block = miner.mine();
+	console.log(`New block added: ${block}`);
+	res.status(302).redirect('/block');
+});
+
+router.get('/public-key', (req, res)=>{
+	res.json({publicKey: wallet.publicKey});
+});
+
+router.get('/balance', (req, res)=>{
+    const balance = wallet.calculateBalance(bc);
+    res.json({'Balance' : balance});
+});
+
+router.post('/transact', (req, res)=>{
+	const { recipient, amount } = req.body;
+	const transaction = wallet.createTransaction(recipient, amount, bc, tp);
+	//console.log(tp, transaction);
+	p2pServer.broadcastTransaction(transaction);
+	res.status(302).redirect('/transaction');
+});
+
+router.post('/mine', (req, res)=>{
+	//console.log(data);
+	const nwBlock = bc.addBlock(req.body.data);
+	console.log(`New block added  ${nwBlock.toString()}`);
+	p2pServer.syncChain();
+	res.status(302).redirect('/block');
+});
+
+p2pServer.listen();
 module.exports = router;
